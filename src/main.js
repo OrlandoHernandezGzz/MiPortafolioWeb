bb7f148e75695844f66f415913cde399437db091
Orlando <orlahdz50@gmail.com>
2023-11-26 17:43:20 -0600

    feat: added fontawesome

--- src/main.js
+++ src/main.js
@@ -4,4 +4,9 @@ import App from './App.vue'
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faBarsStaggered)
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
