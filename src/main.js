be0e8357c6b645a27d3853660d1ad69788e163f6
Orlando <orlahdz50@gmail.com>
2023-11-26 21:26:13 -0600

    feat: added missing libraries fontawesome

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons'
+import { faGithub  } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faBarsStaggered)
+library.add(faGithub)
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
